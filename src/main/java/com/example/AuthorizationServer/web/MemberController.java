0621dc3181c30cf3fb98331d67cfb15cdfebee77
lsung <cp.lsung@gmail.com>
2023-02-23 20:55:46 +0900

    로그인 유지 기능 추가 - 불필요한 코드 삭제

--- src/main/java/com/example/AuthorizationServer/web/MemberController.java
+++ src/main/java/com/example/AuthorizationServer/web/MemberController.java
@@ -5,7 +5,6 @@ import com.example.authorizationserver.domain.member.Member;
 import com.example.authorizationserver.dto.ErrorMsg;
 import com.example.authorizationserver.service.member.MemberService;
 import com.example.authorizationserver.web.dto.MemberLoginRequestDto;
-import com.example.authorizationserver.web.dto.MemberSaveRequestDto;
 import com.example.authorizationserver.web.form.MemberSignUpForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -16,6 +15,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -36,7 +36,14 @@ public class MemberController {
      * @return 로그인 화면
      */
     @GetMapping("/login")
-    public String memberLogin(Model model) {
+    public String login(@RequestParam(value = "error", required = false)String error,
+                        @RequestParam(value = "exception", required = false)String exception,
+                        Model model) {
+
+        // 예외 발생시 알림 표시를 위한 값 설정
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
+
         return "member-login";
     }
 
@@ -102,21 +109,6 @@ public class MemberController {
 
         // 등록완료 화면에 표시할 멤버의 ID를 지정
         model.addAttribute("id", member.getId());
-
-        return "member-signup-complete";
-    }
-
-    /**
-     * 신규 멤버 등록완료 화면으로 이동
-     * @param dto 신규 멤버 등록정보
-     * @param model 모델
-     * @return 신규 멤버 등록완료 화면
-     */
-    @PostMapping("/signup/complete")
-    public String memberSignUpComplete(MemberSaveRequestDto dto, Model model) {
-
-        // 등록완료 화면에 표시할 멤버의 ID를 지정
-        model.addAttribute("id", dto.getId());
 
         return "member-signup-complete";
     }
--- src/main/java/com/example/AuthorizationServer/web/PageController.java
+++ src/main/java/com/example/AuthorizationServer/web/PageController.java
@@ -3,15 +3,30 @@ package com.example.authorizationserver.web;
 import com.example.authorizationserver.config.auth.LoginMember;
 import com.example.authorizationserver.config.auth.dto.SessionMember;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class PageController {
 
     @GetMapping("/")
-    public String index(@LoginMember SessionMember member) {
+    public String index(HttpSession session) {
+
+        // 스프링 시큐리티 컨텍스트 취득
+        Object securityContextObject = session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        if(securityContextObject != null) {
+            SecurityContext securityContext = (SecurityContext)securityContextObject;
+            Authentication authentication = securityContext.getAuthentication();
+            // 인증정보로 부터 멤버 ID를 취득하여 세션에 설정
+            session.setAttribute("member", authentication.getName());
+        }
+
         return "index";
     }
 }
